631d39dad53cc62b9bfa3693bd16997a11845102
Gtec1986 <gtec_01@hotmail.com>
2023-08-23 07:48:02 -0400

    fetch viaCep 0002

--- main.js
+++ main.js
@@ -14,12 +14,23 @@ const cep = document.querySelector('#cep').onkeyup = e => {
      function search() {
           fetch(`https://viacep.com.br/ws/${CEP.value}/json`)
                .then(response => response.json())
-               .then(data => { console.log(data); for (let d in data) { n_data[i] = data[d]; form[i].value = n_data[i]; i = i + 1 } })
-               .catch(err => console.log('deu erro'))
+               .then(data => {
+                    console.log(data); for (let d in data) {
+                         n_data[i] = data[d];
+                         form[i].value = n_data[i]; i = i + 1
+                    }
+                    for (i = 0; i < complements.length; i++) {
+                         if (CEP.value === 'true' || CEP.value === 'não Encontrado') { form[i].value = 'não Encontrado' }
+                    }
+               })
+
+               .catch(err => { if (form.value !== undefined) { console.log('deu erro') } })
+
      }
 
      if (CEP.value !== '' && CEP.value.length === 8 && e.isTrusted === true && e.key === 'Enter') {
           for (i = 0; i > complements.length; i++) { form[i].value = '' }
+
           search()
      }
 
